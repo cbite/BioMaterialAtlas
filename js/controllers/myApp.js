f9007c9c5e428f7b0bba3819f7f0596819b91509
Tim Kuijpers <tjm.kuijpers@gmail.com>
2021-12-16 18:16:24 +0100

    update: show results

--- js/controllers/myApp.js
+++ js/controllers/myApp.js
@@ -1,15 +1,16 @@
 var app = angular.module('BiomaterialAtlas',[]);
 app.controller('FirstController', function($scope,getData,d3Service) {
     $scope.dataisLoaded=false;
-    $scope.loadedData=getData.getjson('data/ALP_BMA_json.json');
+    $scope.loadedData=getData.getjson('data/ALP_BMA.json');
     $scope.loadedDataHeartvalves=getData.getjson("data/BioMaterialAtlas_StudyInformationHeartValves.json");
     $scope.loadedData.then(function(data){
         // load the data to the dataset in scope
         $scope.data=data.data;
         $scope.dataisLoaded=true;
         $scope.dataStudyDescription=$scope.defineStudyDescription($scope);
-        $scope.dataStudyDesign=$scope.defineStudyDesign($scope)
-        console.log($scope.dataStudyDescription.Name)
+        $scope.dataStudyDesign=$scope.defineStudyDesign($scope);
+        $scope.dataStudyResults=$scope.defineStudyResults($scope);
+        console.log($scope.dataStudyResults)
         ;})
 
     $scope.defineSecondaryScreening=function($scope){
@@ -28,5 +29,12 @@ app.controller('FirstController', function($scope,getData,d3Service) {
             StudyDescription=$scope.data.Description;
             return StudyDescription;
         }
+    
+    $scope.defineStudyResults=function($scope){
+        // give an overview of the study results
+        StudyResults=$scope.data.Results;
+        
+        return StudyResults;
+    }
 
     })
