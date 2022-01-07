aca7b9f4b04f84bdb5320ce40b3629d55edfa622
Tim Kuijpers <tjm.kuijpers@gmail.com>
2022-01-07 10:11:13 +0100

    Carousel; TO DO make carousel entry dynamic

--- js/directives/showImages.js
+++ js/directives/showImages.js
@@ -2,15 +2,15 @@ app.directive('showImages',function($timeout){
     return{
         restrict:'E',
         scope:{
-            link:'=',
+            info:'=',
         },
         templateUrl:'js/directives/showRawImages.html',
-        link: function(scope, element) {
-            console.log(element)
-            $timeout(function() {
-              $('.carousel-indicators li',element).first().addClass('active');
-              $('.carousel-inner .item',element).first().addClass('active');
-            });
-        }
+        //link: function(scope, element) {
+         //   console.log(element)
+         //   $timeout(function() {
+         //     $('.carousel-indicators li',element).first().addClass('active');
+          //    $('.carousel-inner .item',element).first().addClass('active');
+           // });
+       // }
     }
 })
